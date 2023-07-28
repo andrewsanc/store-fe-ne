4e06e1926728e483b90188979c8d1b7458522405
Andrew Sanchez <druu831@gmail.com>
2023-07-28 11:08:38 -0700

    retrieval of products from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <>Hello world</>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3000/api/v1/products/", {
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      return await response.json();
+    };
+
+    const products = fetchData();
+    setProducts(products);
+  }, []);
+
+  return (
+    <div className='ui container'>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
